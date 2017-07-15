73487f35cdbc0f9a79c951935db87b5fa815021a
nikhilMucherla <nikhilmucherla04@gmail.com>
2017-07-15 23:25:12 +0530

    modifed scriberegister

--- scriberegister.php
+++ scriberegister.php
@@ -1,90 +1,102 @@
 <?php
-$conn=new mysqli("localhost","root","","eyeandi");
+$conn=new mysqli("localhost","root","","iandeye");
 if($conn->connect_error)
  {
  	 die("Connection failed". $conn->connect_error);
-
  }
- else
- 	echo "Connection"." "."successful";
- if(isset($_POST['register-submit']))
+ if(isset($_POST['submit']))
  {
-   $availability=$_POST['availability'];
-  // echo"availability =" .$x;
+   $email=$_POST['email'];
+   $name=$_POST['name'];
    $location=$_POST['location'];
+   $contact=$_POST['contact'];
    $language=$_POST['language'];
    $qualification=$_POST['qualification'];
-   $email=$_POST['email'];
-   $password=$_POST['password'];
-   $sql="INSERT into scribes(email,password,location,language,availability,qualification)values('$email','$password','$location','$language','$availability','$qualification')";
+   $sql="INSERT into scribe(email,name,contact,location,qualification,language)values('$email','$name','$contact','$location','$qualification','$language')";
    $res=$conn->query($sql);
-if($res)
-  echo" inserted";
-else
-  echo "not inserted";
-
-
-
-
  }
- 
-
-
-?>
-
-
+ ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<h1><p align="center" style="color:#FCFBFA  ;">REGISTRATION FORM</p></h1>
-</head>
-<body background = "blue.jpg">
-<form method="post">
-<br><br><br><br><br>
-  <h3><p align="center" style="color:#FCFBFA;">Email&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="email"></p></h3>
-  <h3><p align="center" style="color:#FCFBFA;">Password&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="password"></p></h3>
-  
-  
-  <h3><p align="center" style="color:#FCFBFA;">Location &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-  
-  <select name ="location" id="location">
-  <option value="hyderabad">hyderabad</option>
-  
-  
+ <head>
+  <title> Scribe Registration</title>
+   <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+           <link rel="stylesheet" type="text/css" href="main.css">
+             <style>
+               $yellow : #fcdab7;
+               .jumbotron {
+               margin-bottom: 0px;
+               background-image: url(red-polygon.jpg);
+               background-position: 0% 25%;
+               background-size: cover;
+               background-repeat: no-repeat;
+               color: white;
+               text-shadow: black 0.3em 0.3em 0.3em;
+               }
+               
+             </style>
+             <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+ <form method="post">
+   <div class="container">
+     <div class="jumbotron">
+       <h1>Scribe Registration</h1>
+     </div>
+     <br></br>
+       <div class="col-sm-8">
+         <div class="col-sm-4">
+  <select name="language">
+  <option value="ENGLISH">ENGLISH</option>
+  <option value="HINDI">HINDI</option>
+  <option value="TELUGU">TELUGU</option>
 </select>
-</p></h3>
-  <h3><p align="center" style="color:#FCFBFA;">Language&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-  
-  <select name ="language" id="language">
-  <option value="hindi">hindi</option>
-  <option value="english">english</option>
-  <option value="telugu">telugu</option>
-  
-</select>
-</p></h3>
-<h3><p align="center" style="color:#FCFBFA;">Availability&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-  
-  <select name ="availability" id="availability"> 
-  <option value="weekend">weekend</option>
-  <option value="weekday">weekday</option>
-  
-  
-</select>
-</p></h3>
-<h3><p align="center" style="color:#FCFBFA;">Qualification&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-  
-  <select name ="qualification" id="qualification">
-  <option value="Btech">Btech</option>
-  <option value="Bcom">Bcom</option>
-  <option value="below grade 10">below grade 10</option>
-  
-</select>
-</p></h3>
+         
+       </div>
+         <div class="col-sm-8">
+           <br>
+             <div class="inputwrap">
+         Name Of Scribe:<br>
+         <input type="text" name ="name">   
+             </div>
+             <br>
 
+               <div class="inputwrap">
+           <!--time format, redo-->
+          Location:<br>
+          <input type="text" name ="location">
+            
+              </div>
+             <br>
 
-  <p align="center"><input type="submit" name="register-submit" value="Register"></p>
-  <p align="center"><a href="login.php">log in?</a></p>
+             <div class="inputwrap">
+          Qualification:<br>
+          <input type="text" name ="qualification">
+            
+              </div>
+             <br>
 
-</from>
+             <div class="inputwrap">
+			 
+			 
+         Contact no:<br>
+         <input type="text" name ="contact">
+           
+           <br>
+             </div>
 
-</body>
+             <div class="inputwrap"> 
+         Email: <br>
+         <input type="text" name ="email">
+		 <br>
+            <input type="submit" name="submit"/>
+           </div>
+           
+       </div>
+     </form>
+   </div>
+ </body>
+</html> 
