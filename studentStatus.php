0008db2cd725979d9fc85299dffe791e0135901f
nikhilMucherla <nikhilmucherla04@gmail.com>
2017-07-16 00:11:49 +0530

    password for student status

--- studentStatus.php
+++ studentStatus.php
@@ -18,13 +18,15 @@ else
 <br><br><br><br><br>
 
 <h3><p align="center" style="color:#FCFBFA;">Email&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="email"></p></h3>
+<h3><p align="center" style="color:#FCFBFA;">Password&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="password" name="password"></p></h3>
+
 <p align="center"><input type="submit" name="admin-submit" value="Register"></p>
 
 <?php
 if(isset($_POST['admin-submit']))
 {
 	$email=$_POST['email'];
-	$sql="SELECT * from student where email='$email' and status=1";
+	$sql="SELECT * from student where email='$email' and status=1 and password='$password";
 	$result=$conn->query($sql);
 	if($result->num_rows==1)
 	{
