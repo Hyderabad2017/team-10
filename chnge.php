b28c2d2a63cc298ee38829982b14a41e848fc58b
Vineelaaari <vineela.aari@gmail.com>
2017-07-16 07:36:38 +0530

    changes
    changes

---
+++ chnge.php
@@ -0,0 +1,44 @@
+<?php
+$conn=new mysqli("localhost","root","","iandeye");
+if($conn->connect_error)
+ {
+ 	 die("Connection failed". $conn->connect_error);
+ }
+ else
+ 	echo "Connection successful";
+  session_start();
+  $email=$_SESSION['email'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<h1><p align="center" style="color:#FCFBFA  ;"><?php echo"Welcome ".$email; ?> </p></h1>
+</head>
+<body background = "blue.jpg">
+<br><br><br><br><br>
+
+
+  <form method="post" >
+ 
+  
+<p align="center"><input type="submit" name="display-submit" value="display students"></p>
+<?php
+if(isset($_POST['display-submit']))
+{
+	//$sql="SELECT * from studentregister";
+	$sql2="SELECT * from scribe s,student sd where s.location=sd.location and s.language=sd.language and s.qualification < sd.qualification and s.email='$email'";
+	$result=$conn->query($sql2);
+    while($row=$result->fetch_assoc())
+    {    
+    	echo "</p>".$row['email']. " ".$row['contact']. " ".$row['name']." ".$row['duration'];
+    	
+    	//echo"<a href='acceptrequest.php?email=".$row['email']."'>";
+    	echo"<a href='acceptrequest.php'>";
+    	echo "select</a>";
+    	echo "<br>" ;	  
+    }
+}
+?>
+</form>
+
+</body>
